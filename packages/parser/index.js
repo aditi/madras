7f4a5b1d0030b83891eb4beb9e9c6937af0dea29
Muthu Kumar <muthukumar@thefeathers.in>
2019-10-06 14:13:54 +0530

    (parser:init)

---
+++ packages/parser/index.js
@@ -0,0 +1,39 @@
+const lexer = require("@aditi/lexer");
+
+const asml = require("fs").createReadStream("./demo.asml", "utf8");
+
+const inc = x => x + 1;
+const tail = xs => xs[xs.length - 1];
+const path = p => ox => p.reduce((acc, curr) => acc ? acc[curr] : undefined, ox);
+
+async function convert (tokens) {
+	const pos = [ 0 ];
+	const list = [];
+
+	for await (const token of tokens) {
+		const { type, value } = token;
+		const lead = path(pos.slice(0, -1))(list);
+
+		if (type === "paren") {
+			if (value === "(") {
+				lead[tail(pos)] = [];
+				pos.push(0);
+			} else {
+				pos.pop();
+				pos[pos.length - 1] = inc(tail(pos));
+			}
+		} else if (type === "symbol") {
+			lead.push(value);
+			pos[pos.length - 1] = inc(tail(pos));
+		} else if (type === "number") {
+			lead.push(Number(value));
+			pos[pos.length - 1] = inc(tail(pos));
+		}
+	}
+
+	return list;
+}
+
+(async () => {
+	console.log(JSON.stringify(await convert(lexer(asml)), null, 2));
+})();
---
+++ packages/parser/output.js
@@ -0,0 +1,6 @@
+module.exports = [
+	["print", ["list", ["list", ["list", 1, 2, 3], 2, 3], 2, 3], 2, 3],
+	["print", 2],
+	["print", 3],
+	["reduce", "add", 0, ["list", 1, 2, 3, 4]],
+];
